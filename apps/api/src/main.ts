4344f47c1d2d0a33b3ad127de60fc31839cc455e
baarbi3 <ayan.whiz@gmail.com>
2026-08-03 22:20:10 +0500

    main.ts nest boilerplate

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  app.setGlobalPrefix('api'); // Makes sure the request is coming from /api/...
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  ); // Helps strip unrequired fields from incoming requests
+
+  app.enableCors({
+    origin: process.env.WEB_URL ?? 'localhost:3001',
+    credentials: true,
+  }); // Lets frontend and backend communicate efficiently
+
+  const port = process.env.PORT ?? '3000';
+  await app.listen(port);
+
+  console.log(`API running on http://${port}/api`);
+
+
 }
 void bootstrap();
