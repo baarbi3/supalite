fbb5a68d2d2787f73f1b30591713e58038d8d5b3
baarbi3 <ayan.whiz@gmail.com>
2026-08-05 14:34:54 +0500

    organizations schema

---
+++ apps/api/src/db/schema/organizations.ts
@@ -0,0 +1,29 @@
+import { pgEnum, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { users } from "./users";
+
+export const orgRoleEnum = pgEnum('org_role', ['admin', 'developer']);
+
+export const organizations = pgTable('organizations', {
+  id: uuid().defaultRandom().primaryKey(),
+  name: text('name').notNull(),
+  slug: text('slug').notNull().unique(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
+export const orgMembers = pgTable(`org_members`, {
+  id: uuid().defaultRandom().primaryKey(),
+  orgId: uuid()
+  .notNull()
+  .references(() => organizations.id, { onDelete: 'cascade' }),
+  userId: uuid('user_id').notNull()
+  .references(() => users.id, {onDelete: 'cascade'}),
+  role: orgRoleEnum('role').notNull().default('developer'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+});
+
+export type Organization = typeof organizations.$inferSelect;
+export type NewOrganization = typeof organizations.$inferInsert;
+
+export type OrgMember = typeof orgMembers.$inferSelect;
+export type NewOrgMember = typeof orgMembers.$inferInsert;
