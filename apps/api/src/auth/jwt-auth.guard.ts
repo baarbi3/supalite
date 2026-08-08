7bd5cb97d60ee3bcd193539b022a5bcc0fd191ab
baarbi3 <ayan.whiz@gmail.com>
2026-08-08 15:01:57 +0500

    JWT Guard for token verification

---
+++ apps/api/src/auth/jwt-auth.guard.ts
@@ -0,0 +1,35 @@
+import { CanActivate,ExecutionContext, Injectable, UnauthorizedException,  } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { Request } from "express";
+import { COOKIE_KEYS } from "@supalite/constants";
+import type { JwtPayload } from "@supalite/types";
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor (
+    private jwtService: JwtService,
+    private configService: ConfigService
+  ){}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest<Request>();
+
+    const token = request.cookies?.[COOKIE_KEYS.ACCESS_TOKEN] as string | undefined;
+
+    if (!token) {
+      throw new UnauthorizedException('Not authenticated');
+    }
+
+    try {
+      const payload = this.jwtService.verify<JwtPayload>(token, {
+        secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+      });
+
+      request['user'] =payload;
+      return true;
+    } catch {
+      throw new UnauthorizedException('Invalid or expired token');
+    }
+  }
+}
