820cae842d9112c563b8b57b151b1058e6811a63
baarbi3 <ayan.whiz@gmail.com>
2026-08-09 13:48:46 +0500

    Auth Module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DBModule } from './db/db.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { DBModule } from './db/db.module';
       isGlobal: true,
     }),
     DBModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
